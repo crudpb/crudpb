5ec80866fdd5d23a264bc6b0de76d6df869a7f2f
Flaviano Flauber <flauberjp@gmail.com>
2025-11-16 13:17:59 -0300

    fixing cache

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const listaPrincipal = document.getElementById('listaPrincipal');
 // Load communities data
 async function loadCommunities() {
     try {
-        const response = await fetch('./data/communities.json');
+        const response = await fetch('./data/communities.json?v=20251116');
         const data = await response.json();
         allCommunities = data.communities;
         renderCommunities(allCommunities);
